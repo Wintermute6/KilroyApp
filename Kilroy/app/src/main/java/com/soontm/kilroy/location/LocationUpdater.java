44839826b954f514fa093e37ef45cdf88daab73a
Wintermute6 <hannes.up@gmail.com>
2015-08-26 23:04:30 +0200

    implemented LocationListener in LocationUpdater

--- Kilroy/app/src/main/java/com/soontm/kilroy/location/LocationUpdater.java
+++ Kilroy/app/src/main/java/com/soontm/kilroy/location/LocationUpdater.java
@@ -1,7 +1,31 @@
 package com.soontm.kilroy.location;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
 /**
  * Created by Hannes on 26.08.2015.
  */
-public class LocationUpdater {
+public class LocationUpdater implements LocationListener {
+    @Override
+    public void onLocationChanged(Location location) {
+        
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }

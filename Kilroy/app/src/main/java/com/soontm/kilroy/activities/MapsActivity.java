313fed8edce91d086d68a86aa55e15ea4458a43c
Wintermute6 <hannes.up@gmail.com>
2015-08-28 12:44:45 +0200

    Maps activity shows no map

--- Kilroy/app/src/main/java/com/soontm/kilroy/activities/MapsActivity.java
+++ Kilroy/app/src/main/java/com/soontm/kilroy/activities/MapsActivity.java
@@ -1,23 +1,32 @@
 package com.soontm.kilroy.activities;
 
+import android.content.Context;
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.soontm.kilroy.R;
+import com.soontm.kilroy.location.LocationUpdater;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationUpdater.locationUpdateListener{
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private static final int FIX_UPDATE_TIME = 500; // milliseconds
+    private static final int FIX_UPDATE_DISTANCE = 5; // meters
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        requestLocationUpdates();
     }
 
     @Override
@@ -62,5 +71,25 @@ public class MapsActivity extends FragmentActivity {
      */
     private void setUpMap() {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    private void requestLocationUpdates (){
+        LocationUpdater locationUpdater = new LocationUpdater(Context.LOCATION_SERVICE, FIX_UPDATE_TIME, FIX_UPDATE_DISTANCE, this);
+        locationUpdater.setLocationUpdateListener(this);
+        locationUpdater.requestLocationUpdates();
+    }
+
+    @Override
+    public void onLocationUpdateReceived(Location location) {
+        if(location != null) {
+            updateMap(location);
+        }
+    }
+
+    private void updateMap(Location location){
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude));
+        mMap.animateCamera(cameraUpdate);
     }
 }
